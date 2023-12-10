622094cf988844a0ca7b3e11222efa3b73da1041
benshabbat <benshabbat27@gmail.com>
2023-12-10 21:47:29 +0200

    sheet.iter_rows(values_only=True):

--- Example1/example1.py
+++ Example1/example1.py
@@ -1,3 +1,7 @@
 import openpyxl
 
-book = openpyxl.load_workbook("data.xlsx")
+book = openpyxl.load_workbook(r'C:\Users\bensh\OneDrive\שולחן העבודה\pythonWithExcel\Example1\data.xlsx')
+sheet = book["Sheet"]
+
+for row in sheet.iter_rows(values_only=True):
+    print(row)
