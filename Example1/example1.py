2ff3ccd4fd95c23dd41b0eae54385ed6d99436b3
benshabbat <benshabbat27@gmail.com>
2023-12-10 21:48:23 +0200

    # Get all Data

--- Example1/example1.py
+++ Example1/example1.py
@@ -3,5 +3,6 @@ import openpyxl
 book = openpyxl.load_workbook(r'C:\Users\bensh\OneDrive\שולחן העבודה\pythonWithExcel\Example1\data.xlsx')
 sheet = book["Sheet"]
 
+# Get all Data
 for row in sheet.iter_rows(values_only=True):
     print(row)
