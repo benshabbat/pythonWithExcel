590f13f0cd8b4372ac596d25105fb6c625998a0f
benshabbat <benshabbat27@gmail.com>
2023-12-10 22:03:32 +0200

    # Get all Data

--- Example2/example2.py
+++ Example2/example2.py
@@ -2,6 +2,7 @@ import pandas as pd
 
 df = pd.read_excel(r'C:\Users\bensh\OneDrive\שולחן העבודה\pythonWithExcel\Example1\data.xlsx')
 
-results = df.columns
+# Get all Data
+results = df.iloc[:]
 
 print(results)
