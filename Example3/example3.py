b6b8c295e4afb81720342d518ec103db4ee13d51
benshabbat <benshabbat27@gmail.com>
2023-12-12 23:55:34 +0200

    fix fetch

--- Example3/example3.py
+++ Example3/example3.py
@@ -1,7 +1,7 @@
 import xlwings as xw
-import pandas as pd
+# import pandas as pd
 
-wk = xw.books.open(r'C:\Users\bensh\OneDrive\שולחן העבודה\pythonWithExcel\Example3\data.xlsx')
+wk = xw.Book(r'C:\Users\bensh\OneDrive\שולחן העבודה\pythonWithExcel\Example3\data.xlsx')
 sheet = wk.sheets("Sheet")
-rg= sheet.range("A1:B1")
+rg= sheet.range("A1:B3")
 print(rg.value)
