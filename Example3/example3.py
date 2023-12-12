1ad991fb5a4634b780010a71a7152d422276f026
benshabbat <benshabbat27@gmail.com>
2023-12-12 23:35:34 +0200

    TEST

--- Example3/example3.py
+++ Example3/example3.py
@@ -1,5 +1,7 @@
 import xlwings as xw
 import pandas as pd
 
-wk = xw.books.open(r'C:\Users\bensh\OneDrive\שולחן העבודה\pythonWithExcel\Example2\data.xlsx')
+wk = xw.books.open(r'C:\Users\bensh\OneDrive\שולחן העבודה\pythonWithExcel\Example3\data.xlsx')
 sheet = wk.sheets("Sheet")
+rg= sheet.range("A1:B1")
+print(rg.value)
