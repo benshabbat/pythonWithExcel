154d7aa37e120f785a670834d177a32e577ac7a7
benshabbat <benshabbat27@gmail.com>
2023-12-13 18:32:44 +0200

    print data frame

--- Example3/example3.py
+++ Example3/example3.py
@@ -1,7 +1,13 @@
 import xlwings as xw
-# import pandas as pd
+import pandas as pd
 
 wk = xw.Book(r'C:\Users\bensh\OneDrive\שולחן העבודה\pythonWithExcel\Example3\data.xlsx')
 sheet = wk.sheets("Sheet")
-rg= sheet.range("A1:B3")
-print(rg.value)
+
+# rg= sheet.range("A1:B3")
+# print(rg.value)
+
+
+df= sheet.range("A1:D3").options(pd.DataFrame).value
+
+print(df)
